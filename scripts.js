9f32335ef5b0ea44f956b4734320c8e9ec6fb0c2
hegeduscili <hegeduscecilia@gmail.com>
2023-09-17 16:22:03 +0200

    elso

---
+++ scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function(){
+    loadView('kezdooldal');
+
+
+    this.window.addEventListener('hashchange', function(){
+        const hash = window.location.hash.substring(1);
+        loadView(hash);
+    });
+})
+
+
+function loadView(viewName){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', viewName + '.html', true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 ){
+            document.getElementById('content').innerHTML = xhr.responseText;
+        }
+    };
+
+    xhr.send();
+}
+
+
